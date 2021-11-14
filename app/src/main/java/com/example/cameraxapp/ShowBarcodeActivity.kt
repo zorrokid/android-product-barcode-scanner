76063d2f49e69d70230c3c82fae8c839dbb039e0
Mikko Keinänen <mikko.keinanen@gmail.com>
2021-11-14 22:57:07 +0200

    Added web search for products

--- app/src/main/java/com/example/cameraxapp/ShowBarcodeActivity.kt
+++ app/src/main/java/com/example/cameraxapp/ShowBarcodeActivity.kt
@@ -2,7 +2,12 @@ package com.example.cameraxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cameraxapp.databinding.ActivityMainBinding
+import android.util.Log
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.cameraxapp.databinding.ActivityShowBarcodeBinding
 
 class ShowBarcodeActivity : AppCompatActivity() {
@@ -17,5 +22,35 @@ class ShowBarcodeActivity : AppCompatActivity() {
         setContentView(view)
         val barcode = intent.getStringExtra(EXTRA_BARCODE)
         binding.textView.apply { text = barcode }
+    }
+
+    fun searchProducts(view: View) {
+        val barcode = binding.textView.text
+
+        // https://html.duckduckgo.com/html/?q=%7Bsearch_terms%7D
+
+        // val intent = Intent(this, )
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://html.duckduckgo.com/html/?q=%7B$barcode%7D"
+
+        Log.d("ShowBarcodeActivity", url)
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                Log.d("ShowBarcodeActivity", "Response is: $response")
+            },
+            Response.ErrorListener { exception ->
+                if (exception != null)
+                    Log.e("ShowBarcodeActivity", exception.toString())
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
     }
 }
